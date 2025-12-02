53f2b687a2dfac69a9fdfb5c309d18c43cf28c93
ShailaAktar <101537372+ShailaAktar@users.noreply.github.com>
2025-12-01 21:55:01 -0700

    Update Dashboard.py

--- src/6DashboardOrNotifier/Dashboard.py
+++ src/6DashboardOrNotifier/Dashboard.py
@@ -49,9 +49,10 @@ def fetch_trades(symbol: str, limit: int = 200) -> pd.DataFrame:
         conn.close()
 
     if not df.empty:
-        df["trade_time"] = pd.to_datetime(df["trade_time"])
-        # we want time moving forward on the x-axis
+
+        df["trade_time"] = pd.to_datetime(df["trade_time"], unit="ms", utc=True)
         df = df.sort_values("trade_time")
+
 
     return df
 
